710a490e4c62bbd3295220588726f712faa9ce84
butters <mhk466@163.com>
2022-06-20 14:30:22 +0800

    update

--- LabelPageTurning/Pods/Target Support Files/Pods-LabelPageTurning/Pods-LabelPageTurning-frameworks.sh
+++ LabelPageTurning/Pods/Target Support Files/Pods-LabelPageTurning/Pods-LabelPageTurning-frameworks.sh
@@ -177,12 +177,10 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ButtersCode/ButtersCode.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYKit/YYKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ButtersCode/ButtersCode.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYKit/YYKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
