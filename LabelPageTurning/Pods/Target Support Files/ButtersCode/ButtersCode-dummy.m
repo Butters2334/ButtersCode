d91ddcb58b75eefdb6cec77af5becdf82f16cc08
butters <mhk466@163.com>
2022-06-20 14:36:33 +0800

    update

--- LabelPageTurning/Pods/Target Support Files/ButtersCode/ButtersCode-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ButtersCode : NSObject
-@end
-@implementation PodsDummy_ButtersCode
-@end
--- LabelPageTurning/Pods/Target Support Files/Pods-LabelPageTurning/Pods-LabelPageTurning-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LabelPageTurning : NSObject
-@end
-@implementation PodsDummy_Pods_LabelPageTurning
-@end
